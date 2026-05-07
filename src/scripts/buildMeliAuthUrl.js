cfc36e98fcfa704613bdff131c64c9081fa94298
Javier Alejandro Garcia <garcia.javier.alejandro@gmail.com>
2026-05-07 13:37:18 -0300

    Update scope in buildMeliAuthUrl to include billing permissions

--- src/scripts/buildMeliAuthUrl.js
+++ src/scripts/buildMeliAuthUrl.js
@@ -4,7 +4,7 @@ const params = new URLSearchParams({
   response_type: 'code',
   client_id: config.meli.appId,
   redirect_uri: config.meli.redirectUri,
-  scope: 'offline_access read write',
+  scope: 'offline_access read write billing',
 });
 
 console.info(`https://auth.mercadolibre.com.ar/authorization?${params.toString()}`);
